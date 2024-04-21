bfac63449062158ad4d3faf7bb2fe4cf1dcee89d
Harry Lee <harrileeeee@gmail.com>
2024-04-21 14:15:37 +0800

    fix: mock payment param rendering

--- src/app/mockPayment/page.js
+++ src/app/mockPayment/page.js
@@ -3,6 +3,7 @@
 import { useSearchParams } from 'next/navigation'
 import Button from '@mui/material/Button'
 import { orderRequester } from '@/utils/requester'
+import { Suspense } from 'react'
 
 export default function Page() {
     const searchParams = useSearchParams()
@@ -11,9 +12,8 @@ export default function Page() {
     const ticket_id = searchParams.get('ticket_id')
     const price = searchParams.get('price')
 
-    orderRequester.defaults.headers.common['Authorization'] = localStorage.getItem('token')
-
     const confirmPay = async () => {
+        orderRequester.defaults.headers.common['Authorization'] = localStorage.getItem('token')
         const res = await orderRequester.patch(`orders/${order_id}`, { action: 'confirm' })
         if (res.status == 200) {
             alert('Payment success!')
@@ -21,20 +21,21 @@ export default function Page() {
         window.location.href = '/order'
     }
 
-    console.log(searchParams)
     return (
-        <div className="page">
-            <h1>Mock Payment System</h1>
-            <p>This page is intended to repalce 3rd party payment system (Google Pay, Apple Pay...)</p>
-            <hr className="my-2" />
-            <h2>Please Confirm Your Payment</h2>
-            <p>Order ID: {order_id}</p>
-            <p>Ticket ID: {ticket_id}</p>
-            <p>Price: ${price}</p>
-            <div className="my-2" />
-            <Button variant="contained" color="primary" onClick={confirmPay}>
-                Pay Now
-            </Button>
-        </div>
+        <Suspense>
+            <div className="page">
+                <h1>Mock Payment System</h1>
+                <p>This page is intended to repalce 3rd party payment system (Google Pay, Apple Pay...)</p>
+                <hr className="my-2" />
+                <h2>Please Confirm Your Payment</h2>
+                <p>Order ID: {order_id}</p>
+                <p>Ticket ID: {ticket_id}</p>
+                <p>Price: ${price}</p>
+                <div className="my-2" />
+                <Button variant="contained" color="primary" onClick={confirmPay}>
+                    Pay Now
+                </Button>
+            </div>
+        </Suspense>
     )
 }
