70aad70bbf6adb43349ae19a799ebc6cd2b4a0dc
Harry Lee <harrileeeee@gmail.com>
2024-04-21 14:50:28 +0800

    fix: event img prefix

--- src/app/event-admin/page.js
+++ src/app/event-admin/page.js
@@ -31,7 +31,7 @@ export default async function EventPage() {
                         </div>
                         <div>
                             <img
-                                src={process.env.NEXT_PUBLIC_BACKEND_EVENT_ROOT + '/images/' + event.imageUri}
+                                src={NEXT_PUBLIC_EVENT_IMG_PREFIX + event.imageUri}
                                 alt={event.eventName}
                                 height="300"
                                 width="400"
