edc32b2fc6772af8450418aeab2607cdb8b0e2a1
Yun Xu <xuyun@berkeley.edu>
2024-04-22 11:10:39 -0400

    Revert "initial profile frontend"
    This reverts commit 514847c4806c07854b049140710c5c44c6788fd8.

--- src/app/profile/component/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-import { Image } from "react-bootstrap";
-
-const Avatar = ({ img = './avatar.png', size = 64 }) => {
-    return (
-        <Image src={img} alt={img} width={size} height={size} thumbnail></Image>
-    )
-}
-
-export default Avatar;
