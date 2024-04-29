6c9c3104c6b177e55cde462fb2bc0889f099ac15
ruixiazS2023 <ruixiaz@andrew.cmu.edu>
2024-04-28 18:25:52 -0700

    delete extra page

--- src/app/review/[topicId]/[rid]/page.js
+++
@@ -1,6 +0,0 @@
-import ReplyList from "@/app/review/ReplyList";
-
-export default function ReplyPage({ params }) {
-  const { topicId, rid } = params;
-  return <ReplyList key={rid} topicId={topicId} rid={rid}/>
-}
--- src/app/review/mementos/[topicId]/[rid]/page.js
+++
@@ -1,6 +0,0 @@
-import MementoList from "@/app/review/history/MementoList";
-
-export default function MementoPage({ params }){
-    const { topicId, rid } = params;
-    return <MementoList topicId={topicId} rid={rid}/>
-}
